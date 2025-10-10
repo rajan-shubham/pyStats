394f56e03fd0e455feabe6fdd2ee6749bb3122ec
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-10-10 23:19:53 +0530

    in, not in, concat(), Upper Lower, left right mid

--- sqlPy/sqlBasics.sql
+++ sqlPy/sqlBasics.sql
@@ -22,4 +22,25 @@ select * from customers order by name desc limit 3, 5;
 -- finding data between particular range, time period, this month data
 -- finding salary whose range is between 1 to 1.5 lakh
 select * from employees order by Salary desc;
-select * from employees where Salary between 50000 and 100000;
+select * from employees where Salary between 35000 and 40000;
+
+
+-- shopping website pe diff type of filters we put
+-- in operator is used for filter while gettng the data from dataset
+select * from employees where Team in ("Marketing", "Client Services");
+select * from employees where Team not in ("Marketing", "Client Services");
+select * from employees where Team in ("Marketing", "Client Services") and Gender in ("Male");
+
+
+-- string functions 1. concat()
+select concat(state," - ", city) as Location from customers;
+select concat_ws(" - ", name, state, city) as Emp_details from customers;
+-- 2. length()
+select length(Salary) as digitCount from employees;
+-- 3. Upper() , Lower()
+select Upper(`First Name`) as Names from employees;
+select Lower(`First Name`) as Names from employees;
+-- 4. left() , right(), mid() finding last 4 char of phonenumber or making username
+select left(name, 4) as username from customers;
+select right(name, 4) as username from customers;
+select mid(name, 2, 5) as username from customers; -- 2 se 5 character in length i want
